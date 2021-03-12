3f57245b4a3060fe4c5b90f7b844c33bb5e1502a
Arsalan Khattak <me@khattak.dev>
2021-03-13 00:14:47 +0500

    feat: typeorm boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiscordModule } from './discord/discord.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [DiscordModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    DiscordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/discord/discord.controller.ts
+++ src/discord/discord.controller.ts
@@ -7,36 +7,48 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { getMongoManager, Repository } from 'typeorm';
 import { DiscordService } from './discord.service';
 import { CreateDiscordDto } from './dto/create-discord.dto';
 import { UpdateDiscordDto } from './dto/update-discord.dto';
-
+import { Discord } from './discord.entity';
 @Controller('discord')
 export class DiscordController {
-  constructor(private readonly discordService: DiscordService) {}
+  constructor(
+    @InjectRepository(Discord)
+    private readonly discordRepository: Repository<Discord>,
+  ) {}
 
   @Post()
-  create(@Body() createDiscordDto: CreateDiscordDto) {
-    return this.discordService.create(createDiscordDto);
+  async create(@Body() createDiscordDto: CreateDiscordDto) {
+    // const manager = getMongoManager();
+    // await manager.save(createDiscordDto);
+    await this.discordRepository.save(createDiscordDto);
+    // return this.discordService.create(createDiscordDto);
   }
 
   @Get()
   findAll() {
-    return this.discordService.findAll();
+    return 'Find All';
+    // return this.discordService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.discordService.findOne(+id);
+    return 'Find One';
+    // return this.discordService.findOne(+id);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateDiscordDto: UpdateDiscordDto) {
-    return this.discordService.update(+id, updateDiscordDto);
+    return 'Update';
+    // return this.discordService.update(+id, updateDiscordDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.discordService.remove(+id);
+    return 'Delete';
+    // return this.discordService.remove(+id);
   }
 }
