ae3084ab275068c35fd8599d36c978051d1ef0a8
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-13 00:47:53 +0200

    fix: update user (#46)
    * fix: Update Discorduser
    
    * Revert "fix: Update Discorduser"
    
    This reverts commit 6e8ac82a96b32f9022d4f336001d64dbd837c77e.
    
    * fix: Update Discorduser

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -62,6 +62,11 @@ export class DiscordService {
       updatedDiscord.socials.github = socials.github;
     }
 
+    const index = this.discord.findIndex(
+      (discordUser) => discordUser.id === id,
+    );
+    this.discord[index] = updatedDiscord;
+
     return 'User updated successfully!';
   }
 
