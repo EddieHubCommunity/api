5e6e1854e18f15f5ba7041eae6ea362ca8d29b3e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-12 20:55:55 +0200

    refactor: ApiTags to Controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('General')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @ApiTags('General')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/discord/discord.controller.ts
+++ src/discord/discord.controller.ts
@@ -12,8 +12,8 @@ import { DiscordService } from './discord.service';
 import { CreateDiscordDto } from './dto/create-discord.dto';
 import { UpdateDiscordDto } from './dto/update-discord.dto';
 
-@Controller('discord')
 @ApiTags('Discord')
+@Controller('discord')
 export class DiscordController {
   constructor(private readonly discordService: DiscordService) {}
 
