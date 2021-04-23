3d08b1cd52677105d3f5b1f9b12216bfcc4ac13d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-23 10:31:26 +0200

    style: added interfaces

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,19 +1,22 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly config: ConfigService) {}
   private readonly logger = new Logger(LoggerMiddleware.name);
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
     try {
       if (JSON.parse(this.config.get('DEBUG'))) {
-        const { body, method, url } = req;
+        const { body, method, originalUrl } = req;
 
         res.on('finish', () => {
           const { statusCode } = res;
           this.logger.log(
-            `${statusCode} | [${method}] ${url} - ${JSON.stringify(body)}`,
+            `${statusCode} | [${method}] ${originalUrl} - ${JSON.stringify(
+              body,
+            )}`,
           );
         });
         next();
