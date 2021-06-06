39f5e38c92e6433878e4344bd16355a5c4684fd1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-06-06 12:28:06 +0200

    fix: update setup-stargate

--- test/step-definitions/requests.ts
+++ test/step-definitions/requests.ts
@@ -1,10 +1,10 @@
 import { binding, given, when, before } from 'cucumber-tsflow';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { exec } from 'child_process';
 import { AppModule } from '../../src/app.module';
 import Context from '../support/world';
 import { ValidationPipe } from '@nestjs/common';
-import { setup, cliOptions } from '@cahllagerfeld/setup-stargate';
 
 @binding([Context])
 export class requests {
@@ -20,14 +20,7 @@ export class requests {
   @before()
   public async before(): Promise<void> {
     if (process.env.STARGATE_BASEURL) {
-      const options: cliOptions = {
-        namespaceUrl: 'http://localhost:8082/v2/schemas/namespaces',
-        authUrl: process.env.STARGATE_AUTH_URL,
-        keyspace: 'eddiehub',
-        password: 'cassandra',
-        username: 'cassandra',
-      };
-      await setup(options);
+      exec('npm run prestart');
     }
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
