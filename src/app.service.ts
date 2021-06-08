89492dfaf45885546655662f85e6abe18b563099
Eddie Jaoude <eddie@jaoudestudios.com>
2021-06-09 00:06:57 +0100

    fix: display current version (#125)

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private config: ConfigService) {}
+
   getHello(): string {
-    return 'Welcome to EddieHub';
+    return `Welcome to EddieHub! Currently running version "${this.config.get(
+      'VERSION',
+    )}"`;
   }
 }
