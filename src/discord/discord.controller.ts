63192392f4a5d338f3fe3031159a03ab6701c29e
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-07-18 17:09:08 +0200

    feat: add new logic to discord module

--- src/discord/discord.controller.ts
+++ src/discord/discord.controller.ts
@@ -9,51 +9,83 @@ import {
   UseGuards,
   HttpCode,
 } from '@nestjs/common';
-import { ApiHeader, ApiSecurity, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 import { TokenGuard } from '../auth/token.strategy';
 import { Author, AuthorObject } from '../auth/author-headers';
 import { DiscordService } from './discord.service';
 import { DiscordDTO } from './dto/discord.dto';
+import { User } from '../auth/decorators/user.decorator';
+import {
+  ScopesDictionary,
+  TokenPayload,
+} from '../auth/interfaces/token-payload.interface';
+import { JWTGuard } from '../auth/jwt.strategy';
+import { ScopesGuard } from '../auth/guards/scopes.guard';
+import { Scopes } from '../auth/decorators/scopes.decorator';
 @ApiTags('Discord')
 @Controller('discord')
 export class DiscordController {
   constructor(private readonly discordService: DiscordService) {}
 
   @Post()
-  @UseGuards(TokenGuard)
-  @ApiSecurity('token')
-  create(@Body() createDiscordDto: DiscordDTO) {
-    return this.discordService.create(createDiscordDto);
+  @UseGuards(JWTGuard, ScopesGuard)
+  @ApiBearerAuth()
+  @Scopes(ScopesDictionary.WRITE)
+  create(@Body() createDiscordDto: DiscordDTO, @User() user: TokenPayload) {
+    return this.discordService.create(createDiscordDto, user.keyspace);
   }
 
   @Get()
-  findAll() {
-    return this.discordService.findAll();
+  @UseGuards(JWTGuard, ScopesGuard)
+  @ApiBearerAuth()
+  @Scopes(ScopesDictionary.READ)
+  findAll(@User() user: TokenPayload) {
+    return this.discordService.findAll(user.keyspace);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.discordService.findOne(id);
+  @UseGuards(JWTGuard, ScopesGuard)
+  @ApiBearerAuth()
+  @Scopes(ScopesDictionary.READ)
+  findOne(@Param('id') id: string, @User() user: TokenPayload) {
+    return this.discordService.findOne(id, user.keyspace);
   }
 
   @Put(':id')
-  @ApiSecurity('token')
-  @UseGuards(TokenGuard)
+  @UseGuards(JWTGuard, ScopesGuard)
+  @ApiBearerAuth()
+  @Scopes(ScopesDictionary.WRITE)
   @ApiHeader({ name: 'User-Uid', required: true })
   update(
     @Param('id') id: string,
     @Body() updateDiscordDto: DiscordDTO,
     @AuthorObject() author: Author,
+    @User() user: TokenPayload,
   ) {
-    return this.discordService.update(id, updateDiscordDto, author);
+    return this.discordService.update(
+      id,
+      updateDiscordDto,
+      author,
+      user.keyspace,
+    );
   }
 
   @Delete(':id')
-  @ApiSecurity('token')
   @HttpCode(204)
   @ApiHeader({ name: 'User-Uid', required: true })
-  @UseGuards(TokenGuard)
-  remove(@Param('id') id: string, @AuthorObject() author: Author) {
-    return this.discordService.remove(id, author);
+  @UseGuards(JWTGuard, ScopesGuard)
+  @ApiBearerAuth()
+  @Scopes(ScopesDictionary.WRITE)
+  remove(
+    @Param('id') id: string,
+    @AuthorObject() author: Author,
+    @User() user: TokenPayload,
+  ) {
+    return this.discordService.remove(id, author, user.keyspace);
   }
 }
