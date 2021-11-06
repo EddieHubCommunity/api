2da9b2e5529cc6a6c5a55b1c8e780e951073c98c
Cahllagerfeld <Cahllagerfeld>
2021-11-06 09:47:16 +0000

    test: close connection

--- src/standup/standup.controller.spec.ts
+++ src/standup/standup.controller.spec.ts
@@ -1,13 +1,19 @@
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MongooseConfigService } from '../environment/mongo-config.service';
 import { Standup, StandupSchema } from './schemas/standup.schema';
 import { StandupController } from './standup.controller';
 import { StandupService } from './standup.service';
+import { Connection } from 'mongoose'
 
 describe('StandupController', () => {
   let controller: StandupController;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close()
+  })
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,6 +32,7 @@ describe('StandupController', () => {
     }).compile();
 
     controller = module.get<StandupController>(StandupController);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
--- src/standup/standup.service.spec.ts
+++ src/standup/standup.service.spec.ts
@@ -1,29 +1,37 @@
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
 import { MongooseConfigService } from '../environment/mongo-config.service';
 import { Standup, StandupSchema } from './schemas/standup.schema';
 import { StandupService } from './standup.service';
 
 describe('StandupService', () => {
   let service: StandupService;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close()
+  })
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [MongooseModule.forRootAsync({
-        useClass: MongooseConfigService,
-      }),
-      MongooseModule.forFeature([
-        { name: Standup.name, schema: StandupSchema },
-      ]),
-      ConfigModule.forRoot({
-        isGlobal: true,
-      }),
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: Standup.name, schema: StandupSchema },
+        ]),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
       ],
       providers: [StandupService],
     }).compile();
 
     service = module.get<StandupService>(StandupService);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
