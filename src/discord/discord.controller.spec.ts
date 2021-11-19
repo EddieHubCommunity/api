bb0f3718e28745eadfc010e4a21387c26929aeb6
Cahllagerfeld <Cahllagerfeld>
2021-11-19 18:24:15 +0000

    test: redefine unit tests

--- src/discord/discord.controller.spec.ts
+++ src/discord/discord.controller.spec.ts
@@ -2,13 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DiscordController } from './discord.controller';
 import { DiscordService } from './discord.service';
 import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
+import { Discord, DiscordSchema } from './schemas/discord.schema';
+import { Connection } from 'mongoose';
 
 describe('DiscordController', () => {
   let controller: DiscordController;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: Discord.name, schema: DiscordSchema },
+        ]),
         ConfigModule.forRoot({
           isGlobal: true,
         }),
@@ -18,6 +33,7 @@ describe('DiscordController', () => {
     }).compile();
 
     controller = module.get<DiscordController>(DiscordController);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
--- src/discord/discord.service.spec.ts
+++ src/discord/discord.service.spec.ts
@@ -1,13 +1,28 @@
 import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
 import { DiscordService } from './discord.service';
+import { Discord, DiscordSchema } from './schemas/discord.schema';
 
 describe('DiscordService', () => {
   let service: DiscordService;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: Discord.name, schema: DiscordSchema },
+        ]),
         ConfigModule.forRoot({
           isGlobal: true,
         }),
@@ -16,6 +31,7 @@ describe('DiscordService', () => {
     }).compile();
 
     service = module.get<DiscordService>(DiscordService);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
