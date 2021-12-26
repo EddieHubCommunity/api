462e264aa2d8d7199215eff85dff6abdd0339f6b
Cahllagerfeld <Cahllagerfeld>
2021-12-26 10:29:32 +0000

    test: update unittests

--- src/profile/profile.controller.spec.ts
+++ src/profile/profile.controller.spec.ts
@@ -1,15 +1,39 @@
+import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
 import { ProfileController } from './profile.controller';
+import { ProfileService } from './profile.service';
+import { ProfileSchema, Profile } from './schemas/profile.schema';
 
 describe('ProfileController', () => {
   let controller: ProfileController;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: Profile.name, schema: ProfileSchema },
+        ]),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
       controllers: [ProfileController],
+      providers: [ProfileService],
     }).compile();
 
     controller = module.get<ProfileController>(ProfileController);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
--- src/profile/profile.service.spec.ts
+++ src/profile/profile.service.spec.ts
@@ -1,15 +1,39 @@
+import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
+import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
+import { Profile, ProfileSchema } from './schemas/profile.schema';
 
 describe('ProfileService', () => {
   let service: ProfileService;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        MongooseModule.forFeature([
+          { name: Profile.name, schema: ProfileSchema },
+        ]),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
+      controllers: [ProfileController],
       providers: [ProfileService],
     }).compile();
 
     service = module.get<ProfileService>(ProfileService);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
