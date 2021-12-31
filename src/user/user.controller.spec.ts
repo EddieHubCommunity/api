b93bf30835b05a1a71d390868d60fbcd31731377
Cahllagerfeld <Cahllagerfeld>
2021-12-31 16:00:01 +0000

    test: unittest

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,15 +1,48 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
+import { GeocodingService } from './geocoding.service';
+import {
+  GithubProfile,
+  GithubProfileSchema,
+  UserModel,
+  UserSchema,
+} from './schema/user.schema';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        HttpModule,
+        MongooseModule.forFeature([
+          { name: UserModel.name, schema: UserSchema },
+          { name: GithubProfile.name, schema: GithubProfileSchema },
+        ]),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
       controllers: [UserController],
+      providers: [GeocodingService, UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,46 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { getConnectionToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Connection } from 'mongoose';
+import { MongooseConfigService } from '../environment/mongo-config.service';
+import { GeocodingService } from './geocoding.service';
+import {
+  GithubProfile,
+  GithubProfileSchema,
+  UserModel,
+  UserSchema,
+} from './schema/user.schema';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let connection: Connection;
+
+  afterEach(async () => {
+    await connection.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      imports: [
+        MongooseModule.forRootAsync({
+          useClass: MongooseConfigService,
+        }),
+        HttpModule,
+        MongooseModule.forFeature([
+          { name: UserModel.name, schema: UserSchema },
+          { name: GithubProfile.name, schema: GithubProfileSchema },
+        ]),
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+      ],
+      providers: [UserService, GeocodingService],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    connection = await module.get(getConnectionToken());
   });
 
   it('should be defined', () => {
