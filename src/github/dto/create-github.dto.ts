c279fa0aca0ac35a09cb9a617e62c92253414a66
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-22 15:48:47 +0000

    chore: remove unnecessary import

--- src/github/dto/create-github.dto.ts
+++ src/github/dto/create-github.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateGithubProfileDTO {
   @IsString()
