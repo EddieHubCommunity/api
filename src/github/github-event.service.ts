638169b7a12bc94511ff9f27c9d65c049d5bd3db
Eddie Jaoude <eddie@jaoudestudios.com>
2022-06-09 17:17:54 +0100

    fix: debugging

--- src/github/github-event.service.ts
+++ src/github/github-event.service.ts
@@ -19,6 +19,7 @@ export class GithubEventService {
   public readonly eventObservable = this.eventSubject.asObservable();
 
   public emitEvent(event): void {
+    console.log(event); // @TODO remove
     this.eventSubject.next(event);
   }
 
