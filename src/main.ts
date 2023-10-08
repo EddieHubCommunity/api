e44155de9ab7fd28910bbf54fe9f26920b787a42
Gurkirat Singh <tbhaxor@gmail.com>
2023-10-08 22:20:09 +0530

    fix(app): changes based on new package version

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 import * as fs from 'fs';
 
 async function bootstrap() {
   // DigitalOcean Apps has cert as environment variable but Mongo needs a file path
   // Write Mongo cert file to disk
   if (process.env.CA_CERT) {
-    fs.writeFileSync('cert.pem', process.env.CA_CERT!);
+    fs.writeFileSync('cert.pem', process.env.CA_CERT);
   }
 
   const app = await NestFactory.create(AppModule);
